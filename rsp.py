45cf2f5d8b7282de2f20c19c2fcb42141d4496ac
BenKwon <kwon5891@gmail.com>
2016-10-25 13:02:32 +0900

    무한플레이 기능추가

--- rsp.py
+++ rsp.py
@@ -3,6 +3,7 @@ Traditional gawi-bawi-bo game
 
 @author SeungJoo Kwon
 @created 12-10-2016
+
 """
 
 import random
@@ -21,16 +22,16 @@ def determine_winner(a,b):
 		print("비겼습니다")
 	else:
 		print("당신이 패배 하였습니다")
-if __name__ == '__main__':
 	"""gawi : 1 bawi: 2 bo: 3"""
+while(True):
+
 	cpu_output_index =[1,2,3]
 	cpu_output =["가위","바위","보"]
+
 	b = random.randint(1,3)
 	x = input("가위:1,바위:2,보:3 중에 하나를 골라주세요 :")
 	a = int(x)
 	print("컴퓨터가 "+cpu_output[b-1]+"를 냈습니다")
 	determine_winner(a,b)
-
-
 
 
